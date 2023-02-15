b0299a0eab143afdf7e794989122a1cfc9b9057d
Yukta269 <96491851+Yukta269@users.noreply.github.com>
2023-02-15 18:29:19 +0000

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+from_dir = "./Downloads"
+to_dir = "./Document_Files"
+
+for i in range(): 
+    list_of_files = os.listdir(from_dir)
+    root,extention = os.path.splitext(from_dir)
+    #print(list_of_files)
+
